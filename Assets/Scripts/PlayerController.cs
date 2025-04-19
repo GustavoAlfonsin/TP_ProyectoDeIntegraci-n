32e7ee23606e3c2d4c2915d7db63ca8680f62158
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2025-04-18 21:22:02 -0300

    Movimiento del personaje y la camara

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //PLAYER
+    private float movementSpeed = 5.0f;
+    private float gravity = -9.81f;
+    private float jumpHeight = 2.0f;
+    private CharacterController controller;
+    private Vector3 playerSpeed;
+
+    //CAMARA
+    private float mouseSensitivity = 5f;
+    private float verticalRotation = 0f;
+    [SerializeField] private float verticalRotationLimit = 45.0f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        moveCharacter();
+        moveCamera();
+    }
+
+    private void moveCamera()
+    {
+        // Rotación horizontal
+        float horizontalMouseRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0,horizontalMouseRotation,0);
+
+        // Rotación Vertical
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation,-verticalRotationLimit, verticalRotationLimit);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation,0,0);
+    }
+
+    private void moveCharacter()
+    {
+        float XMovement = Input.GetAxis("Horizontal");
+        float ZMovement = Input.GetAxis("Vertical");
+        Vector3 movement = transform.right * XMovement + transform.forward * ZMovement;
+        controller.Move(movement * movementSpeed * Time.deltaTime);
+
+        if (controller.isGrounded && playerSpeed.y < 0)
+        {
+            playerSpeed.y = 0f;
+        }
+        playerSpeed.y += gravity * Time.deltaTime;
+        controller.Move(playerSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            playerSpeed.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
