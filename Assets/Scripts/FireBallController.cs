89f55144ab268d592096391218dcc4088aec7e41
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2025-04-25 17:53:35 -0300

    Ultimos detalles

--- Assets/Scripts/FireBallController.cs
+++ Assets/Scripts/FireBallController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class FireBallController : MonoBehaviour
 {
+    private float damage = 20f;
+    private float lifeTime = 7.5f;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<ZombieController>().getDamage(damage);
+        }
+        Debug.Log(other.tag);
+        Destroy(gameObject);
     }
 }
